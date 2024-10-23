3ec351862720a363b22da437c0a834d0fa42e84c
ManuNunez <manu.nunez.2004@gmail.com>
2024-10-23 11:55:39 -0600

    feat(home): module function added

--- app/routes/home/__init__.py
+++ app/routes/home/__init__.py
@@ -1,1 +1,7 @@
+from flask import Flask
 from app.routes.home.home import home_bp
+from app.routes.home.about_us import about_bp
+
+def init_home(app: Flask):
+    app.register_blueprint(home_bp)
+    app.register_blueprint(about_bp)
