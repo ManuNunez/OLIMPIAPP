9aa16f64a8b1ef5a3ae305326e6652f2fe7bf89a
ManuNunez <niuweme.nunez@gmail.com>
2025-01-09 22:33:20 -0600

    feat(user model): __init__ method added

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from . import db
+from .user_role import UserRole
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -11,6 +12,21 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     school_id = db.Column(db.Integer, db.ForeignKey('schools.id'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    def __init__(self, name, curp, username, email, password_hash, school_id):
+        self.name = name
+        self.curp = curp
+        self.username = username
+        self.email = email
+        self.password_hash = password_hash
+        self.school_id = school_id
+
+    def add_default_role(self):
+        default_roles = [10, 11]
+        for role_id in default_roles:
+            user_role = UserRole(user_id=self.id, role_id=role_id)
+            db.session.add(user_role)
+        db.session.commit()
 
     def __repr__(self):
         return f'<User {self.name}>'
