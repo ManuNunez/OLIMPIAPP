a7f420e13a4253c7a91cc78f4c0b554dce388b4a
GDcasillas <guadalupe.casillas5528@alumnos.udg.mx>
2025-02-18 15:44:35 -0600

    new routes for : professors , students, profile

--- app/routes/user/user.py
+++ app/routes/user/user.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template
 
 user_bp = Blueprint('user', __name__)
 
-@user_bp.route('/')
-def index():
-    return render_template('user/index.html')
+@user_bp.route('/profile')
+def profile():
+    return render_template('user/profile.html')
+
+@user_bp.route('/professors')
+def professors():
+    return render_template('user/professors.html')
+
+@user_bp.route('/students')
+def students():
+    return render_template('user/students.html')
